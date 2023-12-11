5fd930cf196d8086ce1d27d7083e1d9c4d5bc3c8
Karel Miarka <karel.miarka@seznam.cz>
2023-12-11 19:20:51 +0100

    Set conn timeout and adds it to constructor

--- src/main/java/io/github/escposjava/print/NetworkPrinter.java
+++ src/main/java/io/github/escposjava/print/NetworkPrinter.java
@@ -8,16 +8,22 @@ public class NetworkPrinter implements Printer {
    private OutputStream printer = null;
    private final String address;
    private final int port;
+   private final int timeout;
 
-   public NetworkPrinter(String address, int port){
+   public NetworkPrinter(String address, int port, int timeout) {
       this.address = address;
       this.port = port;
+      this.timeout = timeout;
    }
 
    public void open(){
       try {
-         Socket socket = new Socket(this.address, this.port);
-         socket.setSoTimeout(1000);
+         Socket socket = new Socket();
+         // This limits the time allowed to establish a connection in the case
+         // that the connection is refused or server doesn't exist.
+         socket.connect(new InetSocketAddress(address, port), timeout);
+         // This stops the request from dragging on after connection succeeds.
+         socket.setSoTimeout(timeout);
          printer = socket.getOutputStream();
       } catch (IOException e) {
          throw new RuntimeException(e);
